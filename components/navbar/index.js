10ae031e66c92eab9390858258ce1ece4ebc56c8
abhinavtyagiO <abhinav_t@pp.iitr.ac.in>
2022-12-08 22:50:56 +0530

    Create Navbar

---
+++ components/navbar/index.js
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  const user = true;
+  const username = true;
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">DEV-DOC</button>
+          </Link>
+        </li>
+
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL} />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {!username && (
+          <li>
+            <Link href="/login">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
